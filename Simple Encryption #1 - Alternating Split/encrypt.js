45e334cd6bab781c0ab3e5d5dcd38c18c9363add
Salah A.Fattah <salahfattah1@outlook.com>
2022-03-26 01:37:19 +0200

    create the decrypt algorithm

--- Simple Encryption #1 - Alternating Split/encrypt.js
+++ Simple Encryption #1 - Alternating Split/encrypt.js
@@ -1,4 +1,7 @@
 function encrypt(text, n) {
+    if(text===""||n<=0 || text===null){
+        return text;
+    }
     let oddStr;
     let evenStr;
     let result;
@@ -18,4 +21,37 @@ function encrypt(text, n) {
     return result;
 }
 
-console.log(encrypt("012345", 2));
+function decrypt(text, n) {
+    
+    let oddStr;
+    let evenStr;
+    let result
+    
+    for(let j=0;j<n;j++){
+        result=[];
+        oddStr="";
+        evenStr="";
+    for(let i=Math.floor(text.length/2);i<text.length;i++){
+
+        evenStr+=text[i];
+        }
+    for(let i=0;i<Math.floor(text.length/2);i++){
+            oddStr+=text[i];}
+    
+        // console.log(oddStr);
+        // console.log(evenStr);
+    
+    for(let i=0;i<evenStr.length;i++){
+        result.push(evenStr[i]);
+        result.push(oddStr[i]);
+    }
+     text=result.join("");
+}
+    
+
+    return text;
+}
+
+
+
+console.log(decrypt("s eT ashi tist!", 2));
