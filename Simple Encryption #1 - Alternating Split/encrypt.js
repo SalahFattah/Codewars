12779fa7c67ccf0dd6aa0c3796ac20504a2d1ef2
Salah A.Fattah <salahfattah1@outlook.com>
2022-03-25 22:58:32 +0200

    Implement the encrypt algorithm and test it n times

--- Simple Encryption #1 - Alternating Split/encrypt.js
+++ Simple Encryption #1 - Alternating Split/encrypt.js
@@ -1,6 +1,10 @@
 function encrypt(text, n) {
-    let oddStr="";
-    let evenStr="";
+    let oddStr;
+    let evenStr;
+    let result;
+    for(let j=0;j<n;j++){
+        oddStr="";
+        evenStr="";
     for(let i=0;i<text.length;i++){
         if(i%2===0){
             evenStr+=text[i];
@@ -8,7 +12,10 @@ function encrypt(text, n) {
             oddStr+=text[i];
         }
     }
-    return oddStr+evenStr;
+     result=oddStr+evenStr;
+     text=result;
+}
+    return result;
 }
 
-console.log(encrypt("012345", 1));
+console.log(encrypt("012345", 2));
