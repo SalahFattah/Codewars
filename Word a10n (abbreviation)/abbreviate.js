d55f69c11c0d2bd29a48becc6c5313e7bcfbb1f7
Salah A.Fattah <salahfattah1@outlook.com>
2022-03-25 19:56:29 +0200

    Made a loop and test it on the simplest form of the problem

---
+++ Word a10n (abbreviation)/abbreviate.js
@@ -0,0 +1,30 @@
+/*The word i18n is a common abbreviation of internationalization in the developer
+ community, used instead of typing the whole word and trying to spell it correctly.
+ Similarly, a11y is an abbreviation of accessibility.
+
+Write a function that takes a string and turns any and all "words" (see below)
+within that string of length 4 or greater into an abbreviation, following these rules:
+
+-A "word" is a sequence of alphabetical characters. 
+By this definition, any other character like a space or hyphen (eg. "elephant-ride") 
+will split up a series of letters into two words (eg. "elephant" and "ride").
+-The abbreviated version of the word should have the first letter,
+ then the number of removed characters, then the last letter (eg. "elephant ride" => 
+ "e6t r2e").
+*/
+
+
+function abbreviate(string){
+    // let alpha="abcdefghijklmnopqrstuvwxyz";
+    let strArr=string.split(" ");
+    let newArr=[];
+    for(let i of strArr){
+        if (i.length>=4){
+            let slicedString=i.slice(1,i.length-1);
+            let word=`${i[0]}${slicedString.length}${i[i.length-1]}`;
+            newArr.push(word);
+        }
+    }
+    console.log(newArr);
+}
+abbreviate("internationalization");
