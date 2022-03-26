6b6b46586f9a00fcbbf1c2fac4e1537d49be6b47
Salah A.Fattah <salahfattah1@outlook.com>
2022-03-26 14:58:31 +0200

    Shorten code by using built in methods for binary conversion

--- IPv4 to int32/ipToInt32.js
+++ IPv4 to int32/ipToInt32.js
@@ -4,20 +4,24 @@ function ipToInt32(ip){
     let wholeNum='';
     for(let i=0;i<num.length;i++ ){
         let str='';
-        let secondStr='';
+        // let secondStr='';
         num[i]=+(num[i]);
+        str=num[i].toString(2);
+        if(str.length<8){
+            str=str.padStart(8,0);
+        }
 
-        while(num[i]>0){
-            str+=num[i]%2;
-            num[i]=Math.floor(num[i]/2);
-        }
-        for(let i=str.length-1;i>=0;i--){
-            secondStr+=str[i];
-        }
-        if(secondStr.length<8){
-            secondStr=secondStr.padStart(8,0);
-        }
-        wholeNum+=secondStr;
+        // while(num[i]>0){
+        //     str+=num[i]%2;
+        //     num[i]=Math.floor(num[i]/2);
+        // }
+        // for(let i=str.length-1;i>=0;i--){
+        //     secondStr+=str[i];
+        // }
+        // if(secondStr.length<8){
+        //     secondStr=secondStr.padStart(8,0);
+        // }
+        wholeNum+=str;
         
     }
     wholeNum=+("0b"+wholeNum);
@@ -26,4 +30,4 @@ function ipToInt32(ip){
   }
 
 
-  ipToInt32("128.32.10.1");
+  console.log(ipToInt32("128.32.10.1"));
