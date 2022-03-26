c09f2a35fdab7d9eb41015d6b7b8b59ed9dba6cf
Salah A.Fattah <salahfattah1@outlook.com>
2022-03-26 14:31:49 +0200

    Convert each number to binary

--- IPv4 to int32/ipToInt32.js
+++ IPv4 to int32/ipToInt32.js
@@ -2,9 +2,21 @@ function ipToInt32(ip){
     //...
     let num=ip.split(".");
     for(let i=0;i<num.length;i++ ){
+        let str='';
+        let secondStr='';
         num[i]=+(num[i]);
+        while(num[i]>0){
+            str+=num[i]%2;
+            num[i]=Math.floor(num[i]/2);
+        }
+        for(let i=str.length-1;i>=0;i--){
+            secondStr+=str[i];
+        }
+        
+        // console.log(secondStr);
     }
-    console.log(num)
+    // console.log(num)
   }
+
 
   ipToInt32("128.32.10.1");
