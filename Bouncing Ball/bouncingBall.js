53bd77b88ee356b06e78c05b98b5f3dc8d797eb6
Salah A.Fattah <salahfattah1@outlook.com>
2022-03-26 15:58:06 +0200

    Add the three condisions for the experiment and counter for falling

---
+++ Bouncing Ball/bouncingBall.js
@@ -0,0 +1,14 @@
+function bouncingBall(h,  bounce,  window) {
+    // your code here
+    if(!(h>0 && (bounce>0 && bounce<1)&& window<h)){
+        return -1
+    }
+    let count=0;
+    do{
+        count++
+        h=h*bounce;
+    }while(h>window);
+
+    return count;
+  }
+  console.log(bouncingBall(10.0, 0.66, 1.5));
