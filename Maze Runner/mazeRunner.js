db4ac85d82f0eb70b8db08c2fa87a76d6f7aaae5
Salah A.Fattah <salahfattah1@outlook.com>
2022-03-26 12:19:00 +0200

    add switch statement to update current postion according to the directions

--- Maze Runner/mazeRunner.js
+++ Maze Runner/mazeRunner.js
@@ -5,6 +5,8 @@ function mazeRunner(maze, directions) {
     let finishPosition;
     let startRow=0;
     let finishRow=0;
+    let currentPosition;
+    let currentRow;
     for(let arr of maze){
         for (let i=0;i<arr.length;i++){
             if(arr[i]===2){
@@ -16,13 +18,81 @@ function mazeRunner(maze, directions) {
 
             }
         }
-       
     }
+    currentPosition=startPosition;
+    currentRow=maze[startRow];
+    let rowIndex=startRow;
+    // console.log(currentPosition);
+    // console.log(currentRow)
+    while(true){
+        for(let i of directions){
+            switch(i){
+                case "N":
+                    rowIndex--;
+                    currentRow=maze[rowIndex];
+                    
+                    if(currentRow[startPosition]===0){
+                        console.log("safe");
+                    }else if(currentRow[startPosition]===3){
+                        console.log("finish");
+                    }else{
+                        console.log("dead");
+                    }
+                    
+                    // console.log(currentPosition);
+                    
+                    break;
+                case "S":
+                    rowIndex++;
+                    currentRow=maze[rowIndex];
+                    
+                    if(currentRow[startPosition]===0){
+                        console.log("safe");
+                    }else if(currentRow[startPosition]===3){
+                        console.log("finish");
+                    }else{
+                        console.log("dead");
+                    }
+                        
+                        break;
+                case "E":
+                    currentPosition++;
+                    if(currentRow[currentPosition]===0){
+                        console.log("safe");
+                    }else if(currentRow[currentPosition]===3){
+                        console.log("finish");
+                    }else{
+                        console.log("dead");
+                    }
+                    
+                    
+                    // console.log(currentPosition);
+                    
+                    break;
+                case "W":
+                    currentPosition--;
+                    if(currentRow[currentPosition]===0){
+                        console.log("safe");
+                    }else if(currentRow[currentPosition]===3){
+                        console.log("finish");
+                    }else{
+                        console.log("dead");
+                    }
+                    
+                    // console.log(currentPosition);
+                    
+                    break;
+            }
+        }
+        break;
+    }
+
+
     // console.log(width);
-    console.log(startPosition);
-    console.log(finishPosition);
-    console.log(finishRow);
-    console.log(startRow);
+    // console.log(startPosition);
+    // console.log(finishPosition);
+    // console.log(finishRow);
+    // console.log(startRow);
   }
 
   var maze = [[1,1,1,1,1,1,1],
@@ -32,4 +102,4 @@ function mazeRunner(maze, directions) {
             [1,0,1,0,1,0,1],
             [1,0,0,0,0,0,1],
             [1,2,1,0,1,0,1]];
-mazeRunner(maze,["N","N","N","N","N","E","E","E","E","E"]);
+mazeRunner(maze,["N","N","N","N","N","E","E","S","S","E","E","N","N","E"]);
